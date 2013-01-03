2276edbe09d9f9676d818b03347850083e272ee5
azureru <eru.saputra@gmail.com>
2013-01-03 19:36:53 +0100

    Install OH-MY-ZSH

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 cd "$(dirname "${BASH_SOURCE}")"
 git pull
+
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
 }
+
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
 else
@@ -13,5 +15,10 @@ else
 		doIt
 	fi
 fi
+
+# install oh-my-zsh
+curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+
+# do it!
 unset doIt
 source ~/.bash_profile
